9a60106cf18964326b1ea5bd269f9e365c6a171a
Tobinpadavil <139835059+Tobinpadavil@users.noreply.github.com>
2024-11-23 10:48:48 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,6 @@
+function openLinks() {
+    var urls = ['https://cpa.becker.com/unit/B-03/V4.1/unitView', 'https://learn.simandhareducation.com/site/dashboard', 'https://mail.google.com/mail/u/0/#inbox' , 'https://www.perplexity.ai/search/a4de0ec4-19a5-4eb3-865b-863a6aeb698c' , 'https://poe.com/Claude-instant' , 'https://nies.terradotta.com/index.cfm?FuseAction=Security.AngLogin'];
+    for (var i = 0; i < urls.length; i++) {
+      window.open(urls[i], '_blank');
+    }
+  }
